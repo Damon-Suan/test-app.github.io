586a157aa62f81b1e40f07759a2254dd163103ff
DamonSuan <190977024@qq.com>
2023-11-13 11:12:52 +0800

    首页左侧

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style lang = "less">
-  heml,body,h3 {
+  heml,body,h3,p {
     margin: 0;
     padding: 0;
   }
--- src/components/common-aside.vue
+++ src/components/common-aside.vue
@@ -7,27 +7,33 @@
     :collapse="isCollapse"
     background-color="#545c64"
     text-color="#fff"
-    active-text-color="#ffd04b">
-  
-    <h3>{{ isCollapse ? '后台' : '通用后台管理系统' }}</h3>
-    <el-menu-item @click="clickMenu(item)" v-for="item in noChildren" :key="item.name" :index="item.name">
+    active-text-color="#ffd04b"
+  >
+    <h3>{{ isCollapse ? "后台" : "通用后台管理系统" }}</h3>
+    <el-menu-item
+      @click="clickMenu(item)"
+      v-for="item in noChildren"
+      :key="item.name"
+      :index="item.name"
+    >
       <i :class="`el-icon-${item.icon}`"></i>
-      <span slot="title">{{item.label}}</span>
+      <span slot="title">{{ item.label }}</span>
     </el-menu-item>
-    <el-submenu  v-for="item in hasChildren" :key="item.label" :index="item.label">
+    <el-submenu
+      v-for="item in hasChildren"
+      :key="item.label"
+      :index="item.label"
+    >
       <template slot="title">
         <i :class="`el-icon-${item.icon}`"></i>
-        <span slot="title">{{item.label}}</span>
+        <span slot="title">{{ item.label }}</span>
       </template>
-      <el-menu-item-group  v-for="subItem in item.children" :key="subItem.name"> 
-
-        <el-menu-item @click="clickMenu(subItem)" :index="subItem.name">{{subItem.label}}</el-menu-item>
-
+      <el-menu-item-group v-for="subItem in item.children" :key="subItem.name">
+        <el-menu-item @click="clickMenu(subItem)" :index="subItem.name">{{
+          subItem.label
+        }}</el-menu-item>
       </el-menu-item-group>
-   
     </el-submenu>
-    
-  
   </el-menu>
 </template>
 
@@ -37,7 +43,7 @@
   min-height: 400px;
 }
 .el-menu {
-  height: 100vh;
+  height: 120vh;
   border-right: none;
   h3 {
     color: #fff;
@@ -95,7 +101,7 @@ export default {
             },
           ],
         },
-      ]
+      ],
     };
   },
   methods: {
@@ -107,21 +113,24 @@ export default {
     },
     clickMenu(item) {
       console.log(item);
-      if(this.$route.path !==item.path && !(this.$route.path === '/home' && item.path === '/')){
-        this.$router.push(item.path)
+      if (
+        this.$route.path !== item.path &&
+        !(this.$route.path === "/home" && item.path === "/")
+      ) {
+        this.$router.push(item.path);
       }
-    }
+    },
   },
   computed: {
     noChildren() {
-      return this.menuData.filter(item => !item.children)
+      return this.menuData.filter((item) => !item.children);
     },
     hasChildren() {
-      return this.menuData.filter(item => item.children)
+      return this.menuData.filter((item) => item.children);
     },
     isCollapse() {
-      return this.$store.state.tab.isCollapse  
-    }
-  }
+      return this.$store.state.tab.isCollapse;
+    },
+  },
 };
 </script>
