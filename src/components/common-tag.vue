5b4ae88d3fc64ee4ef94599dd29420605b23907e
DamonSuan-NoteBook <190977024@qq.com>
2023-11-22 17:56:38 +0800

    tag标签

--- src/components/common-tag.vue
+++ src/components/common-tag.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="tabs">
     <el-tag
-      v-for="item in tags"
+      v-for="(item,index) in tags"
       :key="item.path"
       :closable="item.name !== 'home'"
-      :effect="$route.name === item.name ? 'dark' : 'plain'">
+      :effect="$route.name === item.name ? 'dark' : 'plain'"
+      @click="changeMenu(item,index)"
+      @close="handleClose(item,index)"
+      size="small"
+      >
       {{ item.label }}
     </el-tag>
   </div>
@@ -12,7 +16,7 @@
 </template>
 
 <script>
-import { mapState } from "vuex"
+import { mapState , mapMutations } from "vuex"
 export default {
   name:"common-tag",
   data() {
@@ -25,6 +29,34 @@ export default {
       tags: state =>state.tab.tabsList
     })
   },
+  methods: {
+    //点击tag跳转功能
+    changeMenu(item) {
+      if (
+        this.$route.path !== item.path &&
+        !(this.$route.path === "/home" && item.path === "/")
+      ) {
+        this.$router.push({name:item.name})
+      } 
+    },
+    //点击tag删除的功能
+    ...mapMutations(['closeTag']),
+    handleClose(item , index) {
+      //调用store中的mutations
+      this.closeTag(item)
+      const length = this.tags.length//获取的是删除之后的长度
+      if(item.name !== this.$route.name) {
+        return
+      }
+      if(index === length) {
+        this.$router.push({name:this.tags[index -1].name})
+      } else {
+        this.$router.push({
+          name:this.tags[index].name
+        })
+      }
+    }
+  },
   mounted () {
     console.log(this.tags);
   }
@@ -32,6 +64,12 @@ export default {
 
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.tabs{
+  padding: 20px;
+  .el-tag {
+    margin-right: 15px;
+    cursor: pointer;
+  }
+}
 </style>
--- src/store/tab.js
+++ src/store/tab.js
@@ -22,12 +22,17 @@ export default {
             console.log(val,'val');
             //判断添加的数据是否为首页
             if(val.name !=='home') {
+                //***findIndex返回索引位置，如果没有返回-1
                 const index = state.tabsList.findIndex(item =>item.name === val.name)
                 //如果不存在
                 if(index === -1) {
                     state.tabsList.push(val)
                 }
             }
+        },
+        closeTag(state,item) {
+            const index = state.tabsList.findIndex(val => val.name === item.name)
+            state.tabsList.splice(index,1)
         }
     }
 }
