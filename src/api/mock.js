7d063996bf608696b366558d0de6032d68bbc6ad
DamonSuan-NoteBook <190977024@qq.com>
2023-11-28 17:16:47 +0800

    user

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,5 +1,10 @@
 import Mock from 'mockjs'
 import homeApi from './mockServeData/home'
+import user from './mockServeData/user'
 
 //定义mock请求拦截
-Mock.mock('/api/home/getData',homeApi.getStatisticalData)
+Mock.mock('/api/home/getData', homeApi.getStatisticalData)
+Mock.mock('/api/user/add', 'post', user.createUser)
+Mock.mock('/api/user/edit', 'post', user.updateUser)
+Mock.mock('/api/user/del', 'post', user.deleteUser)
+Mock.mock(/api\/user\/getUser/,user.getUserList)
--- src/views/user.vue
+++ src/views/user.vue
@@ -16,8 +16,8 @@
         </el-form-item>
         <el-form-item label="性别" prop="sex">
           <el-select v-model="form.sex" placeholder="请选择">
-            <el-option label="男" value="1"></el-option>
-            <el-option label="女" value="0"></el-option>
+            <el-option label="男" :value="0"></el-option>
+            <el-option label="女" :value="1"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="出生日期" prop="birth">
@@ -25,6 +25,7 @@
             v-model="form.birth"
             type="date"
             placeholder="选择日期"
+            value-format="yyyy-MM-DD"
           >
           </el-date-picker>
         </el-form-item>
@@ -38,14 +39,70 @@
       </span>
     </el-dialog>
     <div class="manage-header">
-      <el-button type="primary" @click="dialogVisible = true">
+      <el-button type="primary" @click="handleAdd()">
         +新增
       </el-button>
+      <el-form :inline="true" :model="userForm">
+        <el-form-item>
+          <el-input placeholder="请输入名称" v-model="userForm.name"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit()">查询</el-button>
+        </el-form-item>
+       
+      </el-form>
+    </div>
+    <div class="common-table">
+      <el-table
+        stripe
+        height="90%"
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="name"
+          label="姓名">
+        </el-table-column>
+        <el-table-column
+          prop="age"
+          label="年龄">
+        </el-table-column>
+        <el-table-column
+          prop="sex"
+          label="性别">
+            <template slot-scope="scope">
+              <span>{{ scope.row.sex == 1 ? '女' : '男' }}</span>
+            </template>
+        </el-table-column>
+        <el-table-column
+          prop="birth"
+          label="出生日期">
+        </el-table-column>
+        <el-table-column
+          prop="addr"
+          label="地址">
+        </el-table-column>
+        <el-table-column
+          prop="addr"
+          label="操作">
+           <template slot-scope="scope">
+              <el-button sizi="mini" @click="handleEdit(scope.row)"> 编辑 </el-button>
+              <el-button sizi="mini" type="danger" @click="handleDelete(scope.row)"> 删除 </el-button>
+           </template>
+        </el-table-column>
+      </el-table>
+      <div class="pager"> 
+        <el-pagination
+          layout="prev, pager, next"
+          :total="total"
+          @current-change = "handlePage">
+        </el-pagination>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { getUser, addUser, editUser, delUser } from '../api'
 export default {
   name: "user",
   data() {
@@ -65,6 +122,16 @@ export default {
         birth: [{ required: true, message: "请选择出生日期" }],
         addr: [{ required: true, message: "请输入地址" }],
       },
+      tableData: [],
+      modalType: 0,//0新增，1编辑
+      total: 0,
+      pageData: {
+        page: 1,
+        limit: 10
+      },
+      userForm: {
+        name:''
+      }
     };
   },
   methods: {
@@ -72,9 +139,17 @@ export default {
     submit() {
       this.$refs.form.validate((valid) =>{
         if(valid) {
-
-          this.dialogVisible = false
+          if(this.modalType === 0) {
+            addUser(this.form).then(() => {
+              this.getList()
+            })
+          }else {
+            editUser(this.form).then(() => {
+              this.getList()
+            })
+          }
         }
+          this.dialogVisible = false
       })
     },
     //关闭
@@ -84,10 +159,75 @@ export default {
     },
     cencel() {
       this.handleClose()
+    },
+    handleEdit(row) {
+      this.modalType = 1
+      this.dialogVisible = true
+      this.form = JSON.parse(JSON.stringify(row))
+    },
+    handleDelete(row) {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          delUser({id:row.id}).then(() =>{
+            this.$message({
+            type: 'success',
+            message: '删除成功!'
+            });
+            this.getList()
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+    },
+    handleAdd() {
+      this.modalType = 0
+      this.dialogVisible = true
+    },
+    getList() {
+       //获取列表数据
+      getUser({ params:{...this.userForm,...this.pageData} }).then(({data})=> {
+        this.tableData = data.list
+        // this.total = data.list ? data.list.length : 0
+        this.total = data.count || 0
+      })
+    },
+    handlePage(val) {
+      this.pageData.page = val
+      this.getList()
+    },
+    //列表查询
+    onSubmit() {
+      this.getList()
     }
+  },
+  mounted() {
+   this.getList()
   }
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.manage {
+  height: 90%;
+  .manage-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .common-table {
+    position: relative;
+    height: calc(100% - 62px);
+    .pager {
+      position: absolute;
+      bottom: 0;
+      right: 20px;
+    }
+  }
+}
 </style>
